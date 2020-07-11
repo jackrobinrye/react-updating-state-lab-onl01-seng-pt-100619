9e8240602fc796ac5b0c6c6b40164d792e2c75a3
Jack <jackrrye@gmail.com>
2020-07-10 18:43:30 -0600

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,34 @@
 // Code DigitalClicker Component Here
+// In the components/DigitalClicker.js file, create a DigitalClicker React component.
+
+// This component has an initial state property called timesClicked, which is initially defined as 0.
+
+// The component renders out a button with a label that shows the timesClicked value. This means that, at the start, your button should just say 0.
+
+// Whenever the button is clicked, update the state by incrementing the timesClicked by 1.
+
+import React, {Component} from "react"
+
+export default class DigitalClicker extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            buttonClicked: false,
+            timesClicked: 0
+        }
+    }
+    
+    thisButtonClicked = () => {
+        let clicks = this.state.timesClicked + 1
+        this.setState({
+            timesClicked: clicks,
+            buttonClicked: true
+        })
+    }
+
+    render() {
+        return (<button onClick={this.thisButtonClicked}>{this.state.timesClicked}</button>)
+    }
+
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,65 @@
 // Code YouTubeDebugger Component Here
+
+// This component has several state properties. The initial state shape looks like this:
+// {
+//     errors: [],
+//     user: null,
+//     settings: {
+//       bitrate: 8,
+//       video: {
+//         resolution: '1080p'
+//       }
+//     }
+//   }
+
+
+
+
+
+import React, {Component} from "react"
+
+export default class YouTubeDebugger extends Component {
+    
+    constructor() {
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    bitrateFunction = () => {
+        this.setState({
+            settings: Object.assign({}, this.state.settings, {
+                bitrate: 12
+            })
+        });
+    }
+
+    resFunction = () => {
+        this.setState({
+            settings: Object.assign({}, this.state.settings, {
+                video: Object.assign({}, this.state.settings.video, {
+                    resolution: '720p'
+                })
+            })
+        });
+    }
+    
+    render() {
+        // Create a button with the class 'bitrate'. Clicking this button changes the settings.bitrate state property to 12.
+        
+        // Create a button with the class 'resolution'. Clicking this button changes the settings.video.resolution state property to '720p'.
+        return (<div>
+            <button className="bitrate" onClick={this.bitrateFunction}>Bitrate</button>
+            <button className="resolution" onClick={this.resFunction}>Resolution</button>
+        </div>)
+    }
+    
+}
